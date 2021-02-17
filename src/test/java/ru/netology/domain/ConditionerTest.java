32f7b4984b8b7163243339bce3f69d5d7ce87e62
Andrey Shitikov <andrey.shitikoff@gmail.com>
2021-02-17 09:43:59 +0300

    Added Test shouldCreate

--- src/test/java/ru/netology/domain/ConditionerTest.java
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -1,5 +1,18 @@
 package ru.netology.domain;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ConditionerTest {
+
+    @Test
+    public void shouldCreate() {
+        Conditioner conditioner = new Conditioner();
+        assertNull(conditioner.name);
+        assertEquals(0, conditioner.maxTemperature);
+        assertEquals(0, conditioner.minTemperature);
+        assertEquals(0, conditioner.currentTemperature);
+        assertFalse(conditioner.on);
+    }
 
 }
