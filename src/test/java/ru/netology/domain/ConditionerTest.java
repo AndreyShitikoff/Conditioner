5c54cb75c9d39f7590906022c3d6ba9d5040c5ee
Andrey Shitikov <andrey.shitikoff@gmail.com>
2021-02-17 10:19:09 +0300

    Added Getter and Setter

--- src/test/java/ru/netology/domain/ConditionerTest.java
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -1,18 +1,14 @@
 package ru.netology.domain;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ConditionerTest {
 
     @Test
-    public void shouldCreate() {
+    public void shouldCGet(){
         Conditioner conditioner = new Conditioner();
-        assertNull(conditioner.name);
-        assertEquals(0, conditioner.maxTemperature);
-        assertEquals(0, conditioner.minTemperature);
-        assertEquals(0, conditioner.currentTemperature);
-        assertFalse(conditioner.on);
     }
 
 }
