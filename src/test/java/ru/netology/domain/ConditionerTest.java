af2db3c98b487890c3a3a91713674c2b3297fad0
Andrey Shitikov <andrey.shitikoff@gmail.com>
2021-02-17 13:59:27 +0300

    Added Test

--- src/test/java/ru/netology/domain/ConditionerTest.java
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -12,11 +12,36 @@ public class ConditionerTest {
     }
 
     @Test
-    public void checkTemperature(){
+    public void shouldIncreaseCurrentTemperature(){
         Conditioner conditioner = new Conditioner();
         assertEquals(0,conditioner.getCurrentTemperature());
         conditioner.setCurrentTemperature(20);
         assertEquals(20,conditioner.getCurrentTemperature());
+
+        conditioner.setCurrentTemperature(30);
+        conditioner.increaseCurrentTemperature();
+        assertEquals(30, conditioner.getCurrentTemperature());
+
+        conditioner.setCurrentTemperature(35);
+        conditioner.increaseCurrentTemperature();
+        assertEquals(30, conditioner.getCurrentTemperature());
+
+    }
+
+    @Test
+    public void shouldDecreaseCurrentTemperature(){
+        Conditioner conditioner = new Conditioner();
+        assertEquals(0,conditioner.getCurrentTemperature());
+        conditioner.setCurrentTemperature(20);
+        assertEquals(20,conditioner.getCurrentTemperature());
+
+        conditioner.setCurrentTemperature(10);
+        conditioner.decreaseCurrentTemperature();
+        assertEquals(10, conditioner.getCurrentTemperature());
+
+        conditioner.setCurrentTemperature(5);
+        conditioner.decreaseCurrentTemperature();
+        assertEquals(10, conditioner.getCurrentTemperature());
 
     }
 
